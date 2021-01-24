f16b6b87bd07c0c3e67c293623bc393c4ec1e7cc
SimonGPort <simon.giroux.portelance@gmail.com>
2021-01-23 22:13:10 -0500

    video youtube

--- src/Original.jsx
+++ src/Original.jsx
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import React, { Component } from 'react'
+import ReactPlayer from "react-player"
 
 class Original extends Component {
   constructor() {
@@ -32,6 +33,7 @@ class Original extends Component {
       mode: this.state.consoleSelection,
       txtSize: this.state.txtSize + "px",
       txtStyle: this.state.txtStyle,
+      url: this.state.url
     }
     let content = [...this.state.content]
     content.push(object)
@@ -103,7 +105,7 @@ class Original extends Component {
       }
       // ici
       if (content.mode === "img") {
-        let imgSrc = this.state.url
+        let imgSrc = content.url
         result = <div style={{
           margin: "20px",
           height: "300px",
@@ -120,6 +122,62 @@ class Original extends Component {
               height: "300px",
               width: "300px",
             }} />
+          </div>
+          <div style={{ display: "flex", width: "45px" }}>
+            <img class="deleteIcon" src="red-x.png" onClick={() => {
+              let textContentTemp = [...this.state.textContent]
+              textContentTemp.splice(idx, 1)
+              let content = [...this.state.content]
+              content.splice(idx, 1)
+              this.setState({ textContent: textContentTemp, content })
+            }} />
+            {idx !== 0 && <img class="deleteIcon" src="arrowUp.png" onClick={() => {
+              let textContentTemp = [...this.state.textContent]
+              let saveTextContent = textContentTemp[idx]
+              textContentTemp.splice(idx, 1)
+              textContentTemp.splice(idx - 1, 0, saveTextContent)
+
+              let content = [...this.state.content]
+              let saveContent = content[idx]
+              content.splice(idx, 1)
+              content.splice(idx - 1, 0, saveContent)
+              this.setState({ textContent: textContentTemp, content })
+            }} />}
+
+            <img class="deleteIcon" src="arrowDown.png" onClick={() => {
+              let textContentTemp = [...this.state.textContent]
+              let saveTextContent = textContentTemp[idx]
+              textContentTemp.splice(idx, 1)
+              textContentTemp.splice(idx + 1, 0, saveTextContent)
+
+              let content = [...this.state.content]
+              let saveContent = content[idx]
+              content.splice(idx, 1)
+              content.splice(idx + 1, 0, saveContent)
+              this.setState({ textContent: textContentTemp, content })
+            }} />
+          </div>
+        </div >
+
+      }
+      // ici
+      if (content.mode === "youtube") {
+        let YTSrc = content.url
+        result = <div style={{
+          margin: "20px",
+          height: "360px",
+          display: "flex"
+        }}>
+
+          <div style={{
+            height: "100%",
+            width: "100%",
+            display: "flex",
+            justifyContent: "center"
+          }}>
+            <ReactPlayer
+              url={YTSrc}
+            />
           </div>
           <div style={{ display: "flex", width: "45px" }}>
             <img class="deleteIcon" src="red-x.png" onClick={() => {
